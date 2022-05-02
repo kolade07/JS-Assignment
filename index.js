f9b565192168e1335941a29870bdbb2c3dadae19
Abisoye Omokolade <68041455+kolade07@users.noreply.github.com>
2022-05-02 19:40:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// Question 1
+let firstArr = [1, 2, 3]
+let a = firstArr[0];
+let b = firstArr[1];
+let c = firstArr[2];
+
+function printSum (){
+   return a + b + c;
+}
+
+console.log(printSum());
+
+
+// Question 2
+let A = [ 75, 80, 70]
+let B = [ 70, 78, 90]
+let Alice = 0;
+let Bob = 0;
+
+for( let i= 0; i <= 3; i++){
+  if( A[i] > B[i]){
+      Alice++
+  } else if(A[i] < B[i] ) {
+      Bob++
+  } else{
+      Alice+=0; 
+     Bob +=0 }
+} 
+
+
+// Question 3
+let ratArr = [-1, 2, 0, 1, 3, 5];
+
+let positive = ratArr[3]/ratArr[4];
+
+parseFloat(positive);
+
+let negative = ratArr[0]/ratArr[1];
+
+parseFloat(negative);
+
+let zero = ratArr[2]/ratArr[1];
+
+parseFloat(zero);
+
+
+//Question 4
+let firstNum = 2;
+let secNum = 3
+
+function firstSum(){
+   return firstNum + secNum;
+} 
+
+
+
+
+
+
+
+ 
+
+
